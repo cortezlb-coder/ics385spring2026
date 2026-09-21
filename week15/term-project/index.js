93de59225aed9b0dd6f05947838e24d89116a590
cortezlb-coder <cortezlb@hawaii.edu>
2026-09-20 20:45:07 -1000

    Update SameSite cookie attribute for production environment

--- week15/term-project/index.js
+++ week15/term-project/index.js
@@ -48,7 +48,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       secure: process.env.NODE_ENV === "production",
       maxAge: 1000 * 60 * 60 * 4
     }
